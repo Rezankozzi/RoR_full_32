0a31b5d60ccb1d5eab47d89ce3500192983a1fbf
Rezankozzi <88398170+Rezankozzi@users.noreply.github.com>
2021-08-22 18:21:16 +0200

    Create class_Train.rb

---
+++ lesson_3/class_Train.rb
@@ -0,0 +1,60 @@
+class Train
+  attr_reader  :speed, :wagons_count, :current_station, :number, :type
+
+  def initialize(number, type, wagons_count)
+    @number = number
+    @type = type
+    @wagons_count = wagons_count
+    @speed = 0
+    @current_station_index = 0
+  end
+
+  def accelrate(volue)
+    @speed +=volue
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def current_station
+    route.station.index(@current_station_index)
+  end
+
+  def next_station
+    route.station.index(@current_station_index + 1)
+  end
+
+  def previous_station
+    route.station.index(@current_station_index - 1)
+  end
+
+  def add_route(route)
+    @route = route
+    @current_station_index = 0
+  end
+
+  def forward
+    @current_station_index += 1
+  end
+
+  def back
+    @current_station_index -= 1
+  end
+
+  def hook_wagon
+    if @speed == 0
+       @wagons_count +=1
+    else
+       puts "Прицепить вагон не возможно! Скорость поезда  должна быть равна 0"
+    end
+  end
+
+  def unhook_wagon
+    if @speed == 0
+       @wagons.count -=1
+    else
+       puts "Отцепить  вагон  не возможно! Скорость поезда  должна быть равна 0"
+    end
+  end
+end
