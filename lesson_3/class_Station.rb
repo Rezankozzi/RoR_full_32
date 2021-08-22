6541bc338a5b0c34550f82a1898be13ae78e0890
Rezankozzi <88398170+Rezankozzi@users.noreply.github.com>
2021-08-22 17:38:44 +0200

    Update and rename rails_logistic.rb to class_Station.rb
    Класс отредактирован с учетом замечаний и сохранен отдельным файлом.

---
+++ lesson_3/class_Station.rb
@@ -0,0 +1,47 @@
+class Station	
+  attr_reader :trains, :name                                                                
+
+  def initialize(name)
+    @name = name	            
+    @trains = []                       
+  end   
+
+  def arrival(train)                 
+    @trains << train
+  end 
+          
+  def departure(train)               
+    @trains.delete(train)    
+  end 
+
+  def trains_by_type(type)          
+    return @trains.select {|train| train.type = type}  
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
