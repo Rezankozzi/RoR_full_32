3495f5da46cd501eca9453f9f7c1e3fc4956a993
Rezankozzi <88398170+Rezankozzi@users.noreply.github.com>
2021-10-21 14:33:00 +0200

    Add files via upload
    В метод validate! добавлена возможность  регистрации нескольких ошибок, которые  потом выводятся при обработке исключения.  Также  формат типа вагона  из метода validate! вынесен в константу класса.

--- lesson_6/wagon/wagon.rb
+++ lesson_6/wagon/wagon.rb
@@ -4,6 +4,7 @@ class Wagon
   attr_reader :type, :name
 
   NUMBER_FORMAT =/^WG-\d{3}$/
+  TYPE_FORMAT = [:cargo, :passenger]
 
   def initialize(number, type)
     @number = number
@@ -14,7 +15,9 @@ class Wagon
   private
 
   def validate!
-  raise "Number has invalid format!"  if @number !~ NUMBER_FORMAT
-  raise "Type has invalid format!" if [:cargo, :passenger].include?(@type) == false
+    errors = []
+    errors << "Number has invalid format!"  if @number !~ NUMBER_FORMAT
+    errors << "Type has invalid format!" if TYPE_FORMAT.include?(@type) == false
+    raise errors.join(' ') unless errors.empty?
   end
 end
