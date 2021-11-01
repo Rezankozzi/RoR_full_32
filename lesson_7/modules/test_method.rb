32ee0bd814f26558404e77450f5b872311f2b85b
Rezankozzi <88398170+Rezankozzi@users.noreply.github.com>
2021-11-01 23:32:54 +0100

    Add files via upload
    Внесены исправления в соответствии с исправлениями в файлах wagon.rb, wagon_cargo.rb  и  wagon_passenger.rb

--- lesson_7/modules/test_method.rb
+++ lesson_7/modules/test_method.rb
@@ -1,7 +1,7 @@
-#   в test_metod  создаются поезда двух типов, к ним прицепляются по пять вагонов соответствующего типа.
+#   в test_method  создаются поезда двух типов, к ним прицепляются по пять вагонов соответствующего типа.
 #   этим поездам назначаются два маршрута-прямой и обратный, состоящие  из двух станций.
 module TestMethod
-  def test_metod
+  def test_method
   station1 = Station.new( "ST#{rand(10..100)}" )
   station2 = Station.new( "ST#{rand(10..100)}" )
   @stations << station1 << station2
@@ -9,14 +9,14 @@ module TestMethod
   route2 = Route.new( station2,station1, "RT-#{rand(100..300)}")
   @routes << route1 << route2
   train1 = Train.new( "TRa-#{rand(20..30)}", :cargo)
-  train2 = Train.new( "TRa-#{rand(20..30)}", :passenger)
+  train2 = Train.new( "TRa-#{rand(31..40)}", :passenger)
   @trains << train1 << train2
   train1.add_route(route1)
   train2.add_route(route2)
   5.times do
   wagon_cargo = CargoWagon.new( "WG-#{rand(100..150)}", [30, 40, 70].sample)
   train1.add_wagon(wagon_cargo)
-  wagon_pass = PassengerWagon.new( "WG-#{rand(100..150)}", [40, 60, 120].sample)
+  wagon_pass = PassengerWagon.new( "WG-#{rand(150..200)}", [40, 60, 120].sample)
   train2.add_wagon(wagon_pass)
   @wagons << wagon_cargo <<  wagon_pass
   end
